2158b067e7cb08c8f9d7fe4bfdec08ccde36fcc1
LobsterAustin <austin@lobstermarketing.com>
2022-04-14 00:19:21 -0400

    testing query from kontent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,63 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Home' />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src='../images/gatsby-astronaut.png'
-      width={300}
-      quality={95}
-      formats={['auto', 'webp', 'avif']}
-      alt='A Gatsby astronaut'
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to='/page-2/'>Go to page 2</Link> <br />
-      <Link to='/using-typescript/'>Go to "Using TypeScript"</Link> <br />
-      <Link to='/using-ssr'>Go to "Using SSR"</Link> <br />
-      <Link to='/using-dsg'>Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-);
+const IndexPage = () => {
+  const data = useStaticQuery(getSettings);
+  const logo = data.kontentItemSettings.elements.footer_logo.value[0]
+  console.log(logo.url)
+
+  return (
+    <Layout>
+      <SEO title='Home' />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <StaticImage
+        src={logo.url}
+        width={logo.width}
+        quality={75}
+        formats={['auto', 'webp', 'avif']}
+        alt='A Gatsby astronaut'
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to='/page-2/'>Go to page 2</Link> <br />
+        <Link to='/using-typescript/'>Go to "Using TypeScript"</Link> <br />
+        <Link to='/using-ssr'>Go to "Using SSR"</Link> <br />
+        <Link to='/using-dsg'>Go to "Using DSG"</Link>
+      </p>
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const getSettings = graphql`
+  query SettingsQuery {
+    kontentItemSettings {
+      id
+      elements {
+        header_logo {
+          value {
+            description
+            height
+            url
+            width
+          }
+        }
+        footer_logo {
+          value {
+            description
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`;
